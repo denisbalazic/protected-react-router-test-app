b52d03a12ac12091ed4eebe77b73cc44c4c8117e
denis balazic <denis.balazic@nobula.eu>
2022-11-18 18:25:51 +0100

    Add ProtectedWrappers to About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,6 +3,7 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import {docco} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import {prRouterContext} from '../router/routerContext';
 import {combinedRouterCode, hierarchyRolesRouterCode, rolesRouterCode, simpleRouterCode} from '../router/routerCode.text';
+import {ProtectedWrapper} from '../protected-react-router/src';
 
 const About = (): ReactElement => {
     const [prRouter] = useContext(prRouterContext);
@@ -27,6 +28,21 @@ const About = (): ReactElement => {
             <SyntaxHighlighter language="jsx" style={docco}>
                 {getRouterCode(prRouter)}
             </SyntaxHighlighter>
+            <ProtectedWrapper isPrivate>
+                <p>this text is for authenticated user only</p>
+            </ProtectedWrapper>
+            <ProtectedWrapper roles={['user']}>
+                <p>this text is for user</p>
+            </ProtectedWrapper>
+            <ProtectedWrapper roles={['admin']}>
+                <p>this text is for admin</p>
+            </ProtectedWrapper>
+            <ProtectedWrapper roles={['superadmin']}>
+                <p>this text is for superadmin</p>
+            </ProtectedWrapper>
+            <ProtectedWrapper roles={['user', 'admin']}>
+                <p>this text is for user and admin</p>
+            </ProtectedWrapper>
         </div>
     );
 };
