2d1a03c9849092699d4f544b2d078f764d13a6e7
denis balazic <denis.balazic@nobula.eu>
2022-11-18 18:24:30 +0100

    Refactor useAuth; fixes bug with 'authenticated' prop

--- src/pseudoStore/authContext.tsx
+++ src/pseudoStore/authContext.tsx
@@ -1,10 +1,14 @@
 import React, {createContext, PropsWithChildren, ReactElement, useMemo, useState} from 'react';
 
-export const authContext = createContext<any[]>([]);
+export const authContext = createContext<any>(null);
 
 const AuthProvider = ({children}: PropsWithChildren): ReactElement => {
-    const [user, setUser] = useState<boolean>(false);
-    const contextValue = useMemo(() => [user, setUser], [user, setUser]);
+    const [authenticated, setAuthenticated] = useState<boolean>(false);
+    const [roles, setRoles] = useState<string[] | undefined>(undefined);
+    const contextValue = useMemo(
+        () => ({authenticated, roles, setAuthenticated, setRoles}),
+        [authenticated, roles, setAuthenticated, setRoles]
+    );
 
     return <authContext.Provider value={contextValue}>{children}</authContext.Provider>;
 };
--- src/pseudoStore/useAuth.ts
+++ src/pseudoStore/useAuth.ts
@@ -2,14 +2,7 @@ import {useContext} from 'react';
 import {authContext} from './authContext';
 
 const useAuth = () => {
-    const [user, setUser] = useContext(authContext);
-
-    const {authenticated, roles} = user;
-
-    const setAuthenticated = (authenticated: boolean): void => setUser({...user, authenticated});
-    const setRoles = (roles: string[] | undefined): void => setUser({...user, roles});
-
-    return {authenticated, roles, setAuthenticated, setRoles};
+    return useContext(authContext);
 };
 
 export default useAuth;
