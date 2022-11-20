27f38a4b7f71aa9ab095952b5e99567f40077d67
Denis Balazic <denisbalazic42@gmail.com>
2022-11-20 18:40:58 +0100

    Style navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,36 +14,42 @@ const Navigation = (): ReactElement => {
 
     return (
         <div className="navigation">
-            <NavLink to="/" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
-                Home
-            </NavLink>
-            <NavLink to="/router" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
-                Router
-            </NavLink>
-            <NavLink to="/dogs" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
-                Dogs
-            </NavLink>
-            <NavLink to="/cats" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
-                Cats
-            </NavLink>
-
-            <p className="navlink">Router: {prRouter}</p>
-
-            {authenticated || roles?.length ? (
-                <>
-                    <div className="navlink" style={{display: 'flex'}}>
-                        <p>Authed:</p>
-                        {roles ? roles.map((role: string) => <p>{role}</p>) : 'no role'}
-                    </div>
-                    <button type="button" className="navlink" onClick={handleLogout}>
-                        Logout
-                    </button>
-                </>
-            ) : (
-                <NavLink to="/login" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
-                    Login
+            <div className="navigation-section">
+                <NavLink to="/" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
+                    Home
                 </NavLink>
-            )}
+                <NavLink to="/router" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
+                    Router
+                </NavLink>
+                <NavLink to="/dogs" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
+                    Dogs
+                </NavLink>
+                <NavLink to="/cats" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
+                    Cats
+                </NavLink>
+            </div>
+
+            <div className="navigation-section">
+                <p className="navlink">Router: {prRouter}</p>
+
+                {authenticated || roles?.length ? (
+                    <>
+                        <div className="navlink" style={{display: 'flex'}}>
+                            <p>Authenticated as:</p>
+                            {roles ? roles.map((role: string) => <p>{role}</p>) : 'no role'}
+                        </div>
+                        <div className="navlink">
+                            <button type="button" onClick={handleLogout}>
+                                Logout
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <NavLink to="/login" end className={({isActive}) => (isActive ? 'navlink active' : 'navlink')}>
+                        Login
+                    </NavLink>
+                )}
+            </div>
         </div>
     );
 };
